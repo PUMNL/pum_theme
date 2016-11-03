6d8d9b28188367502ebe1ec738f0cee5903d630e
ruudgithub <ruudgithub@users.noreply.github.com>
2016-11-03 10:52:29 +0100

    Fixed #3557: Fixed error from log

--- pum/templates/html.tpl.php
+++ pum/templates/html.tpl.php
@@ -18,16 +18,6 @@
         <script src="//html5base.googlecode.com/svn-history/r38/trunk/js/selectivizr-1.0.3b.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.1.0/respond.min.js"></script>
     <![endif]-->
-
-    <link rel="apple-touch-icon" href="/sites/all/themes/pum/apple-touch-icon.png" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/sites/all/themes/pum/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/sites/all/themes/pum/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="/sites/all/themes/pum/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/sites/all/themes/pum/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="/sites/all/themes/pum/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/sites/all/themes/pum/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="/sites/all/themes/pum/apple-touch-icon-152x152.png" />
-
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div class="skip-link">
