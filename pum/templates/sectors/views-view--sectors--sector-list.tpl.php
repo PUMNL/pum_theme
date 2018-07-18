93a37e17a90e077b6fba61d37bf2fcd172a3e94f
ruudgithub <ruudgithub@users.noreply.github.com>
2018-07-18 15:42:39 +0200

    Fixed #1267: Show listview first on expertise page and removed some settings that are not used anymore

--- pum/templates/sectors/views-view--sectors--sector-list.tpl.php
+++ pum/templates/sectors/views-view--sectors--sector-list.tpl.php
@@ -1,21 +1,36 @@
 <script type="text/javascript">
 jQuery(document).ready(function() {
-	var list = ".list-per-sector";
-	var listcivil = ".list-per-sector.civil";
-	var sector = ".sector";
-	var sectorcivil = ".sector.civil";
+  var list = ".list-per-sector";
+  var listcivil = ".list-per-sector.civil";
+  var sector = ".sector";
+  var sectorcivil = ".sector.civil";
 
-	jQuery(".l-banner").addClass("list-view sectors-list-view sector-view");
-	jQuery(list).hide();
-	jQuery(listcivil).show();
-	jQuery(sectorcivil).addClass("active");
+  jQuery(".l-banner").addClass("list-view sectors-list-view sector-view");
+  jQuery('.block-views-sectors-banner-icons').hide();
+  jQuery('.list-per-sector').hide();
+  jQuery('.block-views-sectors-sector-list').show();
+  jQuery(sectorcivil).addClass("active");
 
-	jQuery(sector).click(function() {
-		  jQuery(list).hide();
-		  jQuery(this).next(list).toggle();
-		  jQuery(list).removeClass('active');
-		  jQuery(this).addClass('active');
-	});
+  jQuery('.switch-buttons .icon').click(function() {
+    jQuery('.block-views-sectors-banner-icons').show();
+    jQuery('.block-views-sectors-sector-list').hide();
+    //jQuery(this).next(list).toggle();
+    jQuery('.block-views-sectors-sector-list').removeClass('active');
+    jQuery('.block-views-sectors-banner-icons').addClass('active');
+    jQuery('.list-per-sector').hide();
+  });
+  jQuery('.view-id-sectors .sector').click(function() {
+    jQuery('.list-per-sector').hide();
+    jQuery(this).next(list).toggle();
+  });
+  jQuery('.switch-buttons .list').click(function() {
+    jQuery('.block-views-sectors-banner-icons').hide();
+    jQuery('.block-views-sectors-sector-list').show();
+    //jQuery(this).next(list).toggle();
+    jQuery('.block-views-sectors-banner-icons').removeClass('active');
+    jQuery('.block-views-sectors-sector-list').addClass('active');
+    jQuery('.list-per-sector').hide();
+  });
 });
 </script>
 
